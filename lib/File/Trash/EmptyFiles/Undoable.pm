9b69ed10d67d8a987ae8729044cb6cafb94040c0
perlancar (on pc-home) <perlancar@gmail.com>
2020-06-03 14:06:47 +0700

    Initial commit

---
+++ lib/File/Trash/EmptyFiles/Undoable.pm
@@ -0,0 +1,71 @@
+package File::Trash::EmptyFiles::Undoable;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+use Log::ger;
+
+our %SPEC;
+
+$SPEC{':package'} = {
+    v => 1.1,
+    summary => 'Trash all empty files in the current directory tree, with undo/redo capability',
+    description => <<'_',
+
+This is the undoable version of <pm:App::FileRemoveUtils>
+`delete_all_empty_files`. The CLI is <prog:trash-all-empty-files-u>.
+
+_
+};
+
+$SPEC{trash_all_empty_files} = {
+    v => 1.1,
+    summary => 'Trash all empty (zero-sized) files in the current '.
+        'directory tree, with undo support',
+    args => {
+    },
+    features => {
+        tx => {v=>2},
+        idempotent => 1,
+    },
+};
+sub trash_all_empty_files {
+    require App::FileRemoveUtils;
+    require File::Trash::Undoable;
+
+    my %args = @_;
+
+    my $res = App::FileRemoveUtils::delete_all_empty_files(-dry_run=>1);
+    return $res unless $res->[0] == 200;
+
+    File::Trash::Undoable::trash_files(
+        %args,
+        files => $res->[3]{'func.files'},
+    );
+}
+
+1;
+# ABSTRACT:
+
+=head1 SYNOPSIS
+
+ # use the trash-all-empty-files-u script
+
+
+=head1 DESCRIPTION
+
+This module provides routines to trash all empty files in the current directory
+tree, with undo/redo support. Actual trashing/untrashing is provided by
+L<File::Trash::Undoable>.
+
+
+=head1 SEE ALSO
+
+L<File::Trash::Undoable> and L<trash-u>.
+
+=cut
